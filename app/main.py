242ea9ddece85dbdc212e2f2637b5d2006bf0805
stdll <kxrz00@yahoo.co.jp>
2020-09-28 21:04:01 +0900

    update main

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
 from app.annict.annict import Annict
-from app.models.common_models import RecordRequest
 from app.recorders.epgstation import EpgStation
-from datetime import datetime
 
 
 def main():
     annict = Annict({"token": ""})
     recorders = EpgStation({"endpoint": ""})
 
-    targets = (list(annict.get_record_target()))
-    for t in targets:
-        print(recorders.record_request(t))
+    for t in annict.get_record_target():
+        print(t.title, recorders.record_request(t))
 
 
 if __name__ == '__main__':
