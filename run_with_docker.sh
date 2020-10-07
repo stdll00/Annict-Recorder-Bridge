b18a99ebdc044305c4550c578e80efcbbee51d67
stdll <kxrz00@yahoo.co.jp>
2020-10-07 21:56:49 +0900

    rollback Dockerfile

---
+++ run_with_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export ANNICT_TOKEN=""
+export ENDPOINT=""
+export ANNICT_TOKEN="7Vt5sEE2scAZeYm5H6y3e7NAQimeblGfpRS5T3r4v2g"
+export ENDPOINT="http://shinapuri.local"
+
+docker pull ghcr.io/stdll00/annict-recorder-bridge:latest # auto update
+docker run --rm -e annict_token=$ANNICT_TOKEN -e endpoint=$ENDPOINT \
+      ghcr.io/stdll00/annict-recorder-bridge:latest
